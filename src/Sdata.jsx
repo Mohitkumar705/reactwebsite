ac22202d8697238f4480ea1061a2aa2fa699a468
Mohit Kumar <mohitkumarbsrbsrbsr097@gmail.com>
2024-05-07 15:07:55 +0530

    reactwebsite

---
+++ src/Sdata.jsx
@@ -0,0 +1,41 @@
+import web from "../src/images/s1.jpg";
+import app from "../src/images/app.jpeg";
+import android from "../src/images/android.jpeg";
+import digital from "../src/images/digital.jpeg";
+import marketing from "../src/images/marketing.jpg";
+import software from "../src/images/software.jpeg";
+
+const Sdata = [
+  {
+    imgsrc: web,
+    title: "web development",
+    li:"https://en.wikipedia.org/wiki/Web_development",
+  },
+  {
+    imgsrc: app,
+    title: "App development",
+    li:"https://en.wikipedia.org/wiki/Web_development",
+  },
+  {
+    imgsrc: software,
+    title: "Software development",
+    li:"https://en.wikipedia.org/wiki/Web_development",
+  },
+  {
+    imgsrc: digital,
+    title: "Digital Marketing",
+    li:"https://en.wikipedia.org/wiki/Web_development",
+  },
+  {
+    imgsrc: android,
+    title: "Android development",
+    li:"https://en.wikipedia.org/wiki/Web_development",
+  },
+  {
+    imgsrc: marketing,
+    title: "Marketing",
+    li:"https://en.wikipedia.org/wiki/Web_development",
+  },
+];
+
+export default Sdata;
